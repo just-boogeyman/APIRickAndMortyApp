056c8af8805ad61075a2013d237df84faa57fc78
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-02-25 21:14:49 +0700

    Add .gitignore file

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Ярослав Кочкин on 25.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
