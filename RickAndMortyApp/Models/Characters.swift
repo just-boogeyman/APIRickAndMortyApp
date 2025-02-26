18f2b7f7e2548d80e5a3c5725a5b56ceda088ec8
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-02-26 21:49:06 +0700

    KAN-2 added model

---
+++ RickAndMortyApp/Models/Characters.swift
@@ -0,0 +1,33 @@
+//
+//  Characters.swift
+//  RickAndMortyApp
+//
+//  Created by Ярослав Кочкин on 26.02.2025.
+//
+
+import Foundation
+
+struct Characters: Decodable {
+	let info: Info
+	let results: [Results]
+}
+
+struct Info: Decodable {
+	let count: Int
+	let pages: Int
+	let next: String
+}
+
+struct Results: Decodable {
+	let id: Int
+	let name: String
+	let status: String
+	let species: String
+	let image: String
+	let location: Location
+	let origin: Location
+}
+
+struct Location: Decodable {
+	let name: String
+}
--- RickAndMortyApp/ViewController.swift
+++ RickAndMortyApp/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		view.backgroundColor = .cyan
 		view.backgroundColor = .white
+		fetchCharacters()
+	}
+}
+
+private extension ViewController {
+	func fetchCharacters() {
+		guard let url = URL(string: RickAndMortyAPI.characters.rawValue) else { return }
+		
+		URLSession.shared.dataTask(with: url) { data, _, error in
+			guard let data else {
+				print(error?.localizedDescription ?? "No Error description")
+				return
+			}
+			
+			do {
+				let characters = try JSONDecoder().decode(Characters.self, from: data)
+				print(characters)
+			} catch let error {
+				print(error.localizedDescription)
+			}
+			
+		}.resume()
 	}
 }
 
